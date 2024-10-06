51c50671a635eda69357d3ad75473fd6344a1382
Younghyun Ji <younghyun1@gmail.com>
2024-10-06 23:08:47 +0900

    selection sort

--- src/1_3_selection_sort.c
+++ src/1_3_selection_sort.c
@@ -1,59 +1,45 @@
-#include <stdint.h>
+/* Program 1.4: Selection sort */
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
-#define LEN 200
+#define MAX_SIZE 1000
+#define SWAP(x, y, t) ((t) = (x), (x) = (y), (y) = (t))
+void sort(int [], int); /* selection sort */
 
-uint32_t get_smallest_and_pop(uint32_t* array, size_t* len);
+void main (void) {
+    int i, n;
+    int list[MAX_SIZE];
+    printf("Enter the number of numbers to generate: ");
+    scanf("%d", &n);
 
-int main (void) {
-    srand((unsigned int)time(NULL)); // seed the random number generator
-    clock_t start_time = clock(); // Start timing
-
-    uint32_t* int_array = (uint32_t*) malloc(LEN * sizeof(uint32_t));
-
-    for (size_t i = 0; i < LEN; i++) {
-        int_array[i] = (uint32_t)rand();
+    if (n < 1 || n > MAX_SIZE) {
+        fprintf(stderr, "Improper value of n\n");
+        exit(EXIT_FAILURE);
     }
-
-    uint32_t* sorted_array = (uint32_t*) malloc(LEN * sizeof(uint32_t));
-
-
-    size_t current_len = LEN;
-    for(size_t counter = 0; counter < LEN; counter++) {
-        sorted_array[counter] = get_smallest_and_pop(int_array, &current_len);
+    for (i = 0; i < n; i++) {
+        list[i] = rand() % 1000;
+        printf("%d  ", list[i]);
+        printf("\n");
     }
+    sort(list, n);
 
-    clock_t end_time = clock(); // End timing
-    double time_taken = ((double)(end_time - start_time)) / CLOCKS_PER_SEC * 1000; // Time in milliseconds
-
-    for (size_t i = 0; i < LEN; i++) {
-        printf("%u\n", sorted_array[i]);
-    }
-
-    printf("Total time taken: %.2fms\n", time_taken);
-
-    free(int_array);
-    free(sorted_array);
+    printf("\n Sorted array:\n ");
+    for (i = 0; i < n; i++)
+        printf("%d  ", list[i]);
+    printf("\n");
 }
 
-uint32_t get_smallest_and_pop(uint32_t* array, size_t* len) {
-    uint32_t smallest = UINT32_MAX;
-    size_t smallest_index = 0;
-
-    // Find the smallest element and its index
-    for (size_t i = 0; i < *len; i++) {
-        if (array[i] < smallest) {
-            smallest = array[i];
-            smallest_index = i;
+void sort(int list[], int n) {
+    int i, j, min, temp;
+    for (i = 0; i < n - 1; i++) {
+        min = i;
+        for (j = i + 1; j < n; j++) {
+            if (list[j] < list[min]) {
+                min = j;
+            }
+        }
+        if (i != min) {
+            SWAP(list[i], list[min], temp);
         }
     }
-    
-    for (size_t i = smallest_index; i < *len - 1; i++) {
-        array[i] = array[i + 1];
-    }
-    (*len)--;
-    
-    return smallest;
 }
